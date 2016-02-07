558de7fffae59a184a0784bac94ecc95b1561d80
Jack <steve.a.shogren@gmail.com>
2016-02-07 13:14:33 -0500

    Getting the bug count query to work... cept for limiting

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,24 +17,56 @@ import qualified Data.ByteString.Char8 as BS
 -- key string - pull out an element by key name
 -- . - regular compose, but somehow backwards now..?
 
--- repo_count lang date-range "+forks:\">15\"&per_page=200&page=" num
--- repo_count "https://api.github.com/search/repositories"
+-- https://api.github.com/search/issues?q=label:bug
 
-opts :: String -> String -> Options
-opts lang token = defaults
-                    & param "q" .~ [T.pack $ "language:" ++ lang ++ " forks:\">15\""]
-                    & param "per_page" .~ ["200"]
-                    & param "page" .~ ["1"]
-                           & header "Authorization" .~ [BS.pack $ "token " ++ token]
+authHeader token = header "Authorization" .~ [BS.pack $ "token " ++ token]
+dateRange =  " created:\"2011-01-01..2016-01-30\""
+bugLabel = " label:bug"
 
-query lang = do
+bugSearchOpts repo token = defaults
+                    & param "q" .~ [T.pack $ "repo:" ++ repo ++ dateRange ++ bugLabel]
+                    & authHeader token
+                    & header "User-Agent" .~ ["steveshogren"]
+
+
+repoSearchOpts :: String -> String -> Int -> Options
+repoSearchOpts lang token page = defaults
+                    & param "q" .~ [T.pack $ "language:" ++ lang
+                                      ++ " forks:\">15\"" ++ dateRange]
+                    & param "per_page" .~ ["200"] & param "page" .~ [T.pack . show $ page]
+                    & authHeader token
+                    & header "User-Agent" .~ ["steveshogren"]
+
+bugSearch repo = do
     token <- readFile "token"
-    let options = (opts lang token)
-    print options
-    r <- getWith options "https://api.github.com/search/repositories"
-    let repo_names = r ^.. responseBody . key "items" . values . key "name" . _String
-    let repo_counts = r ^? responseBody . key "total_count" . _Number
-    return $ (repo_names, repo_counts)
+    getWith (bugSearchOpts repo token) "https://api.github.com/search/issues"
+
+repoSearch lang page = do
+    token <- readFile "token"
+    getWith (repoSearchOpts lang token page) "https://api.github.com/search/repositories"
+
+collectTotalCount lang = do
+    r <- repoSearch lang 1
+    return $ r ^? responseBody . key "total_count" . _Number
+
+collectNames :: String -> Int -> IO [String]
+collectNames lang page = do
+    r <- repoSearch lang page
+    return . map T.unpack $ r ^.. responseBody . key "items" . values . key "full_name" . _String
+
+collectAllNames :: String -> IO [String]
+collectAllNames lang =
+    concat <$> (sequence $ map (collectNames lang) [1..10])
+
+collectBugCountsForRepo repo = do
+    bugs <- bugSearch repo
+    return $ bugs ^? responseBody . key "total_count" . _Number
+
+-- looks like search now limits to 30/hr, or 1 every 2min
+-- i want to search 1000 repos per lang... or, 33 hours a lang...
+collectBugCountsForLang lang = do
+     repos <- collectNames lang 1
+     sequence $ map collectBugCountsForRepo repos
 
 someFunc :: IO ()
-someFunc = (query "ruby") >>= print
+someFunc = (repoSearch "ruby" 1) >>= print
