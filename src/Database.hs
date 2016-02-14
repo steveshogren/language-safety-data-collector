60400803927a4b7f9f6f0c637b0c11f7930ce94d
Jack <steve.a.shogren@gmail.com>
2016-02-14 08:13:20 -0500

    Running a formatter over muh code

--- src/Database.hs
+++ src/Database.hs
@@ -1,17 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveGeneric #-}
-module Database (blankRecord
-                , load
-                , clearFile
-                , updateRecord
-                , clearRepoStats
-                , updateRepoBugCount
-                , updateRepoCommitCount
-                , lensIt
-                , bug_count
-                , commit_count
-               ) where
+module Database
+       (blankRecord, load, clearFile, updateRecord, clearRepoStats,
+        updateRepoBugCount, updateRepoCommitCount, lensIt, bug_count,
+        commit_count)
+       where
 
 import Records
 import qualified Data.Map as M
@@ -25,7 +19,10 @@ makeLenses ''Results
 makeLenses ''SeveralResults
 makeLenses ''RepoStat
 
-blankRecord = SeveralResults { _languages = [] }
+blankRecord =
+    SeveralResults
+    { _languages = []
+    }
 
 load :: (Read a) => FilePath -> IO a
 load f = read <$> BS.unpack <$> Str.readFile f
@@ -37,32 +34,43 @@ clearFile :: String -> IO ()
 clearFile f = save blankRecord f
 
 updateRecord f aRecord = do
-  db <- load f
-  save (db & languages <>~ [aRecord]) f
+    db <- load f
+    save (db & languages <>~ [aRecord]) f
 
-blankRepoStats = (M.fromList [("haskell", M.empty)])::RepoStats
+blankRepoStats = (M.fromList [("haskell", M.empty)]) :: RepoStats
 
 clearRepoStats :: String -> IO ()
 clearRepoStats f = save blankRepoStats f
 
 makeNewRepo repoName =
-   RepoStat{_full_name = repoName, _bug_count = Nothing, _commit_count = Nothing}
+    RepoStat
+    { _full_name = repoName
+    , _bug_count = Nothing
+    , _commit_count = Nothing
+    }
 
-updateRepoFieldCount field f lang repoName count  = do
-  db <- load f :: IO(RepoStats)
-  let updated = (at lang . _Just . at repoName . non (makeNewRepo repoName) . field ?~ count) $ db
-  save updated f
+gt :: String -> String -> RepoStats -> RepoStat
+gt lang repoName stats =
+    (stats ^. at lang . non M.empty . at repoName . non (makeNewRepo repoName))
+
+updateRepoFieldCount
+    :: ((a -> Identity (Maybe b)) -> RepoStat -> Identity RepoStat)
+    -> FilePath
+    -> String
+    -> String
+    -> b
+    -> IO ()
+updateRepoFieldCount field f lang repoName count = do
+    db <- load f :: IO (RepoStats)
+    let updated =
+            (at lang . _Just . at repoName . non (makeNewRepo repoName) . field ?~
+             count) $
+            db
+    let updated2 = ((gt lang repoName db) ^. bug_count)
+    save updated f
 updateRepoBugCount = updateRepoFieldCount bug_count
 updateRepoCommitCount = updateRepoFieldCount commit_count
 
 lensIt f lang = do
-  db <- load f :: IO(RepoStats)
-  return $ db ^.at lang . non M.empty
-
--- xx=  updateRepoFieldCount  "repostats" "haskell" "TESTNME" bug_count 2
-
--- type Tester = M.Map Int (M.Map Int Int)
-
--- doer =
---   let x = M.fromList [(1, M.fromList [(1, 4)])] :: Tester
---   in at 1 . non M.empty . at 1 ?~ 3 $ x
+    db <- load f :: IO (RepoStats)
+    return $ db ^. at lang . non M.empty
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,46 +12,57 @@ makeLenses ''SeveralResults
 
 main :: IO ()
 main = do
-  x <- runTestTT tests
-  return ()
+    x <- runTestTT tests
+    return ()
 
 dateRange :: Assertion
-dateRange =  (L.dateRange @?= " created:\"2011-01-01..2016-01-30\"" )
+dateRange = (L.dateRange @?= " created:\"2011-01-01..2016-01-30\"")
 
 testDb = "testing"
 
 blankRecord :: Assertion
-blankRecord =
-   (D.blankRecord^.languages @?= [])
+blankRecord = (D.blankRecord ^. languages @?= [])
 
 clearFileTest :: Assertion
 clearFileTest = do
-  actual <- D.clearFile testDb >> D.load testDb
-  (actual @?= D.blankRecord)
+    actual <- D.clearFile testDb >> D.load testDb
+    (actual @?= D.blankRecord)
 
-aRecord = Results { _total_count = 5, _language = "Haskell"}
+aRecord =
+    Results
+    { _total_count = 5
+    , _language = "Haskell"
+    }
 
 updateFileTest :: Assertion
 updateFileTest = do
-  actual <- D.clearFile testDb >> D.updateRecord testDb aRecord >> D.load testDb
-  ((actual^.languages) @?= [aRecord])
+    actual <-
+        D.clearFile testDb >> D.updateRecord testDb aRecord >> D.load testDb
+    ((actual ^. languages) @?= [aRecord])
 
-makeNewRepo repoName =
-   RepoStat{_full_name = repoName, _bug_count = Nothing, _commit_count = Nothing}
+stat name bug commit =
+    RepoStat
+    { _full_name = name
+    , _bug_count = Just bug
+    , _commit_count = Just commit
+    }
+
+d a b c =
+  a + b + b
 
 saveFieldCount :: Assertion
 saveFieldCount = do
-  actual <- D.clearRepoStats testDb
-              >> D.updateRepoBugCount testDb "haskell" "somerepo" 2
-              >> D.updateRepoCommitCount testDb "haskell" "somerepo" 4
-              >> D.lensIt testDb "haskell"
-  ((actual ^.at "somerepo" . non (makeNewRepo "t") . (D.bug_count)) @?= Just 2)
-  ((actual ^.at "somerepo" . non (makeNewRepo "t") . (D.commit_count)) @?= Just 4)
+    actual <-
+        D.clearRepoStats testDb >>
+        D.updateRepoBugCount testDb "haskell" "somerepo" 2 >>
+        D.updateRepoCommitCount testDb "haskell" "somerepo" 4 >>
+        D.lensIt testDb "haskell"
+    ((actual ^. at "somerepo") @?= Just (stat "somerepo" 2 4))
 
-tests = TestList ["blank record" ~: blankRecord
-                , "date range" ~: dateRange
-                , "update record" ~: updateFileTest
-                , "saving repo stats" ~: saveFieldCount
-                , "clearing and reading from a file" ~: clearFileTest]
-
-
+tests =
+    TestList
+        [ "blank record" ~: blankRecord
+        , "date range" ~: dateRange
+        , "update record" ~: updateFileTest
+        , "saving repo stats" ~: saveFieldCount
+        , "clearing and reading from a file" ~: clearFileTest]
